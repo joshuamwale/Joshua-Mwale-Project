5d0d76c80e588490e30f855a815a81f70734e293
Joshua <mwalejoshua89@gmail.com>
2022-06-24 17:25:15 +0300

    Importing HTML content to the app

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
     displayMovies()
 })
 
-function displayMovies(API);
+displayMovies(movieDetails);
+let API = "https://imdb-api.com/en/API/SearchMovie/k_6rxyo4au/inception%202010"
     //filling in the function
-    function displayMovies(API) {
+    function displayMovies(movieDetails) {
         const response = fetch("https://imdb-api.com/en/API/SearchMovie/k_6rxyo4au/inception%202010");
     const movieDetails = response.json();
     console.log(movieDetails);
     displayMovies(movieDetails.results)
-    }
+    
 
 const movieDetails = document.getElementById("detailed-info");
 const form = document.getElementById("movie-form");
 const searchMovies = document.getElementById("search-movie")
+
+const characterImage = document.getElementById("image")
+characterImage.innerText = movieDetails.results
+ 
+//adding event listeners
+    }
+
