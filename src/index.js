aa68a8cde275a932433efd366ca9b26487303b82
Joshua <mwalejoshua89@gmail.com>
2022-06-24 17:05:45 +0300

    Wrote my index.js functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    searchMovies()
+    displayMovies()
 })
 
-function searchMovies() {
-    fetch()
-}
+function displayMovies(API);
+    //filling in the function
+    function displayMovies(API) {
+        const response = fetch("https://imdb-api.com/en/API/SearchMovie/k_6rxyo4au/inception%202010");
+    const movieDetails = response.json();
+    console.log(movieDetails);
+    displayMovies(movieDetails.results)
+    }
+
+const movieDetails = document.getElementById("detailed-info");
+const form = document.getElementById("movie-form");
+const searchMovies = document.getElementById("search-movie")
