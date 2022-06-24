fbcf132cf01ef20a8e10ce1ab87fd1129210a612
Joshua <mwalejoshua89@gmail.com>
2022-06-24 17:55:57 +0300

    Added event listeners - click and submit

--- src/index.js
+++ src/index.js
@@ -12,13 +12,34 @@ let API = "https://imdb-api.com/en/API/SearchMovie/k_6rxyo4au/inception%202010"
     displayMovies(movieDetails.results)
     
 
-const movieDetails = document.getElementById("detailed-info");
+const results = document.getElementById("detailed-info");
 const form = document.getElementById("movie-form");
 const searchMovies = document.getElementById("search-movie")
 
 const characterImage = document.getElementById("image")
 characterImage.innerText = movieDetails.results
  
-//adding event listeners
+//adding event listeners - click
+searchResults.addEventListener("click", () => {
+    const movieForm = document.getElementById("results")
+
+    let form = document.querySelector("movie-form")
+    addEventListener("submit", (event) => {
+        event.preventDefault()
+        toSearch(event.target.movie-form.value)
+        formReset()
+    })
+
+    function toSearch(event) {
+        let input = document.querySelector("movie-form")
+        let button = document.getElementById("button")
+        input.appendChild("reset-btn")
+        input.textContent = document.querySelector("Reset Search").value
+       
+        let form = document.querySelector("#movie-form")
+        document.querySelector("movie-form").addEventListener("submit", toSearch)
     }
+})
+
+}
 
