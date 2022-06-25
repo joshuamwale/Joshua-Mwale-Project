45acb26fdeb725f40bc571732e7a08532cfe7449
Joshua <mwalejoshua89@gmail.com>
2022-06-25 19:16:55 +0300

    Added Form reset in my index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
-//DOMContentLoaded to ensure DOM is loaded before running my index.js file
+     //DOMContentLoaded to ensure DOM is loaded before running my index.js file
 
 document.addEventListener("DOMContentLoaded", function() {
-    displayAllMovies()
-})
-
-const apiURL = "https://imdb-api.com/en/API/SearchMovie/k_6rxyo4au/inception%202010"
+const apiurl = "https://imdb-api.com/en/API/SearchMovie/k_6rxyo4au/inception%202010"
 
 const imagePath = "https://imdb-api.com/images/original/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_Ratio0.6800_AL_.jpg";
 
-baseUrl(url)
-url = "http://localhost:3000/movies"
-
+let url = "http://localhost:3000/movies"
 
 
 const movieDetails = document.getElementById("detailed-info");
-const form = document.getElementById("movie-form");
-const searchMovies = document.getElementById("search-movie")
+   //DOM Render one movie function
 
-
-//Fetch requests
-//Get Fetch requests for all our movies resources
-
-searchMovies(apiURL);
-async function searchMovies(url) {
-    const response = await fetch(url);
-    const responseData = await response.json();
-    console.log(responseData);
-    displayMovies(responseData.results);
-};
-
-function displayMoviesMovies() {
+function renderOneMovie(movie) {
     movieDetails.innerHTML = "";
     const { resultType, title, description } = movie;
     const movieElement = document.getElementById("results")
@@ -39,28 +21,38 @@ function displayMoviesMovies() {
     alt="${id}"`
 }
 
-//adding event listeners  - click
+const input = document.getElementById("search-movie")
+
+     //adding event listeners  - click
 searchResults.addEventListener("click", () => {
-const movieForm = document.getElementById("results")
+const searchResults = document.getElementById("search results")
 
-//Form reset
-
-let form = document.querySelector("movie-form")
-addEventListener("submit", (event) => {
-event.preventDefault()
-toSearch(event.target.movie-form.value)
-form.reset()
 })
+//Fetch requests
+//Get Fetch requests for all our movies resources
+
+function displayAllMovies() {
+    fetch("http://localhost:3000/movies")
+    .then(response => response.json())
+    .then(movieDetails => movieDetails.forEach(movie => renderOneMovie(movie)))
+    console.log("before fetch returns")
+}
+
+//Initial Render - Render movies to the DOM
+
+function initialize() {
+    displayAllMovies()
+    console.log('after display All Movies')
+    //movieDetails.forEach(movie => renderOneMovie(movie))
+}
+
+initialize()
 
 //Reset search
-function toSearch(event) {
-let input = document.querySelector("movie-form")
-let button = document.getElementById("button")
-input.appendChild("reset-btn")
-input.textContent = document.querySelector("Reset Search").value
-let form = document.querySelector("#movie-form")
-document.querySelector("movie-form").addEventListener("submit", toSearch)
-}
+let form = document.querySelector('form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    toSearch(event.target.movie-form.value)
+    form.reset()
 })
-
-
+});
