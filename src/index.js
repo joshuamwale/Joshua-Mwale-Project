36adaeb132c86f0dfb4211cc35b903ba52551b9d
Joshua <mwalejoshua89@gmail.com>
2022-06-27 09:29:59 +0300

    Re-wrote my fetch functions for clarity

--- src/index.js
+++ src/index.js
@@ -1,45 +1,33 @@
 //DOMContentLoaded to ensure DOM is loaded before running my index.js file
 
 document.addEventListener("DOMContentLoaded", function() {
-    
-//Render one movie
-function renderOneMovie(movie) {
-    movieDetails.innerHTML = "detailed-info";
-    const { resultType, title, description } = movies;
-    const movieElement = document.getElementById("results")
-    movieElement.innerHTML = `
-    <img
-    src="${resultType + title + description}"
-    alt="${id}"`
-}
+    fetchMovies()
 
-const main = document.getElementById("detailed-info")
-const movieImage = document.getElementById("image")
-const movieSearch = document.getElementById("search-movie")
+
+function fetchMovies() {
+    return fetch("https://imdb-api.com/en/API/SearchMovie/k_6rxyo4au/inception%202010")
+    .then((response) => response.json())
+    .then((json) => renderMovies(json));
+}
+    
+//Render movies
+function renderMovies(movies) {
+    const main = document.querySelector("detailed-info")
+    movies.forEach(movie => {
+        const movieImage = document.getElementById("image");
+        movieImage.innerHTML = `
+        <img
+        src="${resultType + title + description}"
+        alt ="${id}"`
+        main.appendChild(movieImage);
+    });
+
+    const movieSearch = document.getElementById("search-movie")
 
 //adding event listeners  - click
-searchBar.addEventListener("click", () => {
+    searchBar.addEventListener("click", () => {
     const searchInput = document.getElementById("search results")
 })
-
-//Fetch requests
-//Get fetch requests for all our movies
-
-function fetchAllMovies() {
-    fetch("https://imdb-api.com/en/API/SearchMovie/k_6rxyo4au/inception%202010")
-    .then(response => response.json())
-    .then(movieDetails=> movieDetails.forEach((movies) => {
-        renderOneMovie(movies)
-    }))
-}
-
-    //Initial render - render movies to the DOM
-
-    function initialize() {
-        fetchAllMovies()
-        //console.log("after fetch returns")
-        //data.forEach(movie => renderOneMovie(movie))
-    }
 
 //Reset search
     let form= document.querySelector('movie-form')
@@ -48,4 +36,5 @@ function fetchAllMovies() {
     toSearch(event.target.movie-form.value)
     form.reset()
     })
+}
 })
